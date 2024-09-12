6c7e42eabb1beff56a322ca9a8c28d0cbf230d7f
Denis Yantarev <denis.yantarev@gmail.com>
2024-09-12 09:57:58 +0100

    Change path to point to PKGBUILD file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [[ ! -z "$1" ]]; then
-    cd "$1"
+if [[ -z "$1" ]]; then
+    PKGBUILD=$1
+else
+    PKGBUILD=./PKGBUILD
 fi
 
-echo "Running makepkg from $PWD"
+echo "Running makepkg for '$PKGBUILD'"
 
-pkg_deps=$(source ./PKGBUILD && echo ${makedepends[@]} ${depends[@]})
+pkg_deps=$(source "$PKGBUILD" && echo ${makedepends[@]} ${depends[@]})
 echo "Installing dependencies: $pkg_deps"
 pacman -Syu --noconfirm $pkg_deps
 
@@ -16,7 +18,7 @@ echo "Running makepkg"
 
 # TODO: support extra flags
 
-su --login builder --command='makepkg -fs ./PKGBUILD'
+su --login builder --command="makepkg -fs $PKGBUILD"
 
 echo "Running namcap"
 
